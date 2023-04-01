c22602b25b0b4e1a5858f9b9a366c1382ed7843b
6yozo <66yozo@gmail.com>
2023-04-01 23:23:08 +0100

    Turn on some features originally used by crkbd

--- rules.mk
+++ rules.mk
@@ -7,10 +7,28 @@
 
 SRC += source.c
 
-# Settings from miryoku
+# Build Options from miryoku
 MOUSEKEY_ENABLE = yes # Maybe not necessary, but does not hurt
-EXTRAKEY_ENABLE = yes # Copy / Paste works because of it
+EXTRAKEY_ENABLE = yes # Copy / Paste works because of it?
 AUTO_SHIFT_ENABLE = yes # Test and disable, I can press shift when needed NP
 # TAP_DANCE_ENABLE = yes # Used by the double tap guard feature
 CAPS_WORD_ENABLE = yes # This is a good feature, test with the desired layout
 KEY_OVERRIDE_ENABLE = yes # Used to activate caps word which probably did not work on qmk master miryoku
+
+# Build Options from crkbd
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover ### i dont need it probably
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
+RGB_MATRIX_ENABLE = no
+RGB_MATRIX_DRIVER = WS2812
+LTO_ENABLE = yes
+# if firmware size over limit, try this option
+# LTO_ENABLE = yes
+RGBLIGHT_SUPPORTED = yes
+RGB_MATRIX_SUPPORTED = yes
